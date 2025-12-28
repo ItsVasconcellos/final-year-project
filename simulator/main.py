0a437ff6f5ec2771f0126caf5fad6bd93451ac3b
ItsVasconcellos <fernando.asv@uol.com.br>
2025-12-28 11:55:28 +0000

    feat: read routes and stations

--- simulator/main.py
+++ simulator/main.py
@@ -1,1 +1,21 @@
-    
+import networkx as nx
+import csv
+
+stations_name_dict = {}
+
+def get_stations():
+    with open(file="../database/stations.csv", mode='r') as stationsFile:
+        stations = csv.DictReader(stationsFile)
+        for station in stations: 
+            stations_name_dict[station["code"]] = station["name"]
+
+def get_routes():
+    with open(file="../database/routes.tsv", mode='r') as routesFile:
+        routes = csv.reader(routesFile, delimiter="\t")
+        for route in routes: 
+            print(route)
+
+
+
+# get_stations()
+get_routes()
