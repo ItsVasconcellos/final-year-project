8140f26b21ff0e2788212f20dcf55158f75a5b7b
ItsVasconcellos <fernando.asv@uol.com.br>
2025-12-28 15:14:01 +0000

    feat: reading files to get routes and stations

--- simulator/main.py
+++ simulator/main.py
@@ -2,6 +2,7 @@ import networkx as nx
 import csv
 
 stations_name_dict = {}
+unique_routes = {}
 
 def get_stations():
     with open(file="../database/stations.csv", mode='r') as stationsFile:
@@ -11,11 +12,37 @@ def get_stations():
 
 def get_routes():
     with open(file="../database/routes.tsv", mode='r') as routesFile:
+        count = 0
         routes = csv.reader(routesFile, delimiter="\t")
-        for route in routes: 
+        for route in routes:
+            if unique_routes.get(''.join(route)):
+                unique_routes[''.join(route)] += 1
+                continue
+            unique_routes[''.join(route)] = 1
+            count+=1
             print(route)
+        print(count)
 
+def avg_value():
+    sum = 0
+    for route in unique_routes.items():
+        sum = sum + route[1]
+    avg = sum/len(unique_routes)
+    print(avg)
 
+def max_value():
+    station = ""
+    maximum = 0
+    for route in unique_routes.items():    
+        if route[1] >= int(maximum):
+            station = route[0]
+            maximum = route[1]
+    print(station)
+    print(maximum)
 
-# get_stations()
-get_routes()
+# avg_value()
+# max_value()
+
+def main():
+    get_stations()
+    get_routes()
