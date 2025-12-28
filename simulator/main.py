312f2a2d755ba3343bc1712a4c2c367d692c50dd
ItsVasconcellos <fernando.asv@uol.com.br>
2025-12-28 19:04:02 +0000

    refactor: separating graph into a new module

--- simulator/main.py
+++ simulator/main.py
@@ -1,18 +1,7 @@
-import networkx as nx
 import csv
+import network
 
-
-def get_stations():
-    stations_code_list = []
-    stations_name_dict = {}
-    with open(file="../database/stations.csv", mode='r') as stationsFile:
-        stations = csv.DictReader(stationsFile)
-        for station in stations:
-            stations_code_list.append(station["code"]) 
-            stations_name_dict[station["code"]] = station["name"]
-    return stations_code_list, stations_name_dict
-
-def get_routes():
+def get_routes() -> dict:
     unique_routes = {}
     with open(file="../database/routes.tsv", mode='r') as routesFile:
         count = 0
@@ -24,14 +13,6 @@ def get_routes():
             unique_routes[''.join(route)] = 1
             count+=1
     return unique_routes
-
-def get_edges():
-    edges_list = []
-    with open(file="../database/edges.csv", mode='r') as connectionFile:
-        edges = csv.DictReader(connectionFile)
-        for edge in edges:
-            edges_list.append([edge["source"],edge["target"],{"weight": edge["distance"]}])
-    return edges_list
 
 # def avg_value():
 #     sum = 0
@@ -50,25 +31,9 @@ def get_edges():
 #     print(station)
 #     print(maximum)
 
-def create_graph():
-    stations, _ = get_stations()
-    edges = get_edges()
-    graph = nx.DiGraph()
-    for station in stations:
-        graph.add_node(station)
-    print(graph.number_of_nodes())
-    print(len(edges))
-    for edge in edges:
-        source, target, weight = edge[0], edge[1], edge[2]
-        graph.add_edge(u_of_edge=source, v_of_edge=target, **weight)
-    print(graph.number_of_edges())
-
-    return graph
-
-
 def main():
-    get_routes()
-    railway_network = create_graph()
+    railway_network = network.create_graph()
+    print(railway_network)
 
 if __name__ == "__main__":
     main()
