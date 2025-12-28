ab727a947f0e0ae1183998b8ff78f3e366125a7d
ItsVasconcellos <fernando.asv@uol.com.br>
2025-12-28 17:10:44 +0000

    feat: create the graph network

--- simulator/main.py
+++ simulator/main.py
@@ -1,16 +1,19 @@
 import networkx as nx
 import csv
 
-stations_name_dict = {}
-unique_routes = {}
 
 def get_stations():
+    stations_code_list = []
+    stations_name_dict = {}
     with open(file="../database/stations.csv", mode='r') as stationsFile:
         stations = csv.DictReader(stationsFile)
-        for station in stations: 
+        for station in stations:
+            stations_code_list.append(station["code"]) 
             stations_name_dict[station["code"]] = station["name"]
+    return stations_code_list, stations_name_dict
 
 def get_routes():
+    unique_routes = {}
     with open(file="../database/routes.tsv", mode='r') as routesFile:
         count = 0
         routes = csv.reader(routesFile, delimiter="\t")
@@ -20,29 +23,52 @@ def get_routes():
                 continue
             unique_routes[''.join(route)] = 1
             count+=1
-            print(route)
-        print(count)
+    return unique_routes
 
-def avg_value():
-    sum = 0
-    for route in unique_routes.items():
-        sum = sum + route[1]
-    avg = sum/len(unique_routes)
-    print(avg)
+def get_edges():
+    edges_list = []
+    with open(file="../database/edges.csv", mode='r') as connectionFile:
+        edges = csv.DictReader(connectionFile)
+        for edge in edges:
+            edges_list.append([edge["source"],edge["target"],{"weight": edge["distance"]}])
+    return edges_list
 
-def max_value():
-    station = ""
-    maximum = 0
-    for route in unique_routes.items():    
-        if route[1] >= int(maximum):
-            station = route[0]
-            maximum = route[1]
-    print(station)
-    print(maximum)
+# def avg_value():
+#     sum = 0
+#     for route in unique_routes.items():
+#         sum = sum + route[1]
+#     avg = sum/len(unique_routes)
+#     print(avg)
 
-# avg_value()
-# max_value()
+# def max_value():
+#     station = ""
+#     maximum = 0
+#     for route in unique_routes.items():    
+#         if route[1] >= int(maximum):
+#             station = route[0]
+#             maximum = route[1]
+#     print(station)
+#     print(maximum)
+
+def create_graph():
+    stations, _ = get_stations()
+    edges = get_edges()
+    graph = nx.DiGraph()
+    for station in stations:
+        graph.add_node(station)
+    print(graph.number_of_nodes())
+    print(len(edges))
+    for edge in edges:
+        source, target, weight = edge[0], edge[1], edge[2]
+        graph.add_edge(u_of_edge=source, v_of_edge=target, **weight)
+    print(graph.number_of_edges())
+
+    return graph
+
 
 def main():
-    get_stations()
     get_routes()
+    railway_network = create_graph()
+
+if __name__ == "__main__":
+    main()
