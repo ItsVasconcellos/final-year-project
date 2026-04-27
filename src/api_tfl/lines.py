1673e64b8cef5b649479caeb0b4eb3a849cac02b
ItsVasconcellos <fernando.asv@uol.com.br>
2026-04-28 00:48:21 +0100

    feat: lines names

---
+++ src/api_tfl/lines.py
@@ -0,0 +1,35 @@
+def get_lines():
+    return [
+        {"type": "elizabeth", "name": "elizabeth"}, # Valid Route
+        {"type": "overground", "name": "liberty"}, # Valid route
+        {"type": "overground", "name": "lioness"}, # Valid Route
+        {"type": "overground", "name": "mildmay"}, # Valid Route
+        {"type": "overground", "name": "suffragette"}, # Valid Route
+        {"type": "overground", "name": "weaver"},
+        # Valid Route
+        {"type": "overground", "name": "windrush"},
+        # Valid Route
+        {"type": "tram",	"name": "tram"},
+        # Valid Route
+        {"type": "tube",	"name": "bakerloo"},
+        # Valid Route
+        {"type": "tube",	"name": "central"},
+        # Valid Route
+        {"type": "tube",	"name": "circle"},
+        # Valid Route
+        {"type": "tube",	"name": "district"},
+        # Valid Route
+        {"type": "tube",	"name": "jubilee"},
+        # Valid Route 
+        {"type": "tube",	"name": "hammersmith-city"}, # Valid Route 
+        {"type": "tube",	"name": "metropolitan"},
+        # Valid Route
+        {"type": "tube",	"name": "northern"},
+        # Valid Route 
+        {"type": "tube",	"name": "piccadilly"},
+        # Valid Route 
+        {"type": "tube",	"name": "victoria"},
+        # Valid Route
+        {"type": "tube",	"name": "waterloo-city"},
+        # Valid Route 
+    ]
